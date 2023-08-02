b4e321f5a6e3aa80b9bef9ff5d167e4d6d718fff
xbronze <zihui_xu@126.com>
2023-08-02 18:46:34 +0800

    first commit

---
+++ java/javaOO/examples/OuterClass.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oracle or the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+public class OuterClass {
+
+    String outerField = "Outer field";
+    static String staticOuterField = "Static outer field";
+
+    class InnerClass {
+        void accessMembers() {
+            System.out.println(outerField);
+            System.out.println(staticOuterField);
+        }
+    }
+
+    static class StaticNestedClass {
+        void accessMembers(OuterClass outer) {
+            // Compiler error: Cannot make a static reference to the non-static
+            //     field outerField
+            // System.out.println(outerField);
+            System.out.println(outer.outerField);
+            System.out.println(staticOuterField);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Inner class:");
+        System.out.println("------------");
+        OuterClass outerObject = new OuterClass();
+        OuterClass.InnerClass innerObject = outerObject.new InnerClass();
+        innerObject.accessMembers();
+
+        System.out.println("\nStatic nested class:");
+        System.out.println("--------------------");
+        StaticNestedClass staticNestedObject = new StaticNestedClass();        
+        staticNestedObject.accessMembers(outerObject);
+        
+        System.out.println("\nTop-level class:");
+        System.out.println("--------------------");
+        TopLevelClass topLevelObject = new TopLevelClass();        
+        topLevelObject.accessMembers(outerObject);                
+    }
+}
